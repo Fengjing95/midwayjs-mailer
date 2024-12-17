f1045e7b536278de87b2dc4b3ebfead15a4b2b56
小枫 <51731411+Fengjing95@users.noreply.github.com>
2024-12-17 15:13:18 +0800

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,10 @@
 import { Options } from 'nodemailer/lib/smtp-connection';
 export * from './dist/index';
 
+// 模板渲染函数接口
+type ITemplateRender = (path: string, env?: Record<string, any>) => Promise<string>;
+
+// 支持的模板渲染的类型
 export enum TemplateType {
   EJS = 'ejs',
   PUG = 'pug',
@@ -11,6 +15,7 @@ export type MailerConfigurationType = {
   prefix?: string;
   customSubjectRender?: (prefix: string, subject: string) => string;
   template?: TemplateType;
+  customRender?: ITemplateRender;
   sender?: string;
 } & Options;
 
