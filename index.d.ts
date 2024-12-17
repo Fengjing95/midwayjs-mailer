df8ad01a4e75c6f7f759d51002f7579e730ae9ce
小枫 <51731411+Fengjing95@users.noreply.github.com>
2024-12-17 15:59:25 +0800

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@ import { Options } from 'nodemailer/lib/smtp-connection';
 export * from './dist/index';
 
 // 模板渲染函数接口
-type ITemplateRender = (path: string, env?: Record<string, any>) => Promise<string>;
+export type ITemplateRender = (path: string, env?: Record<string, unknown>) => Promise<string>;
 
 // 支持的模板渲染的类型
 export enum TemplateType {
